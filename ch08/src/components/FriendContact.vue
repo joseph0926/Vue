8c82694fc9bbb0bd8fc38d2be15387f77ac4fd5c
김영훈 <joseph0926@naver.com>
2022-11-09 08:33:43 +0900

    2022.11.09

---
+++ ch08/src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Show Favorite</button>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNum }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNum", "emailAddress"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // default: (value) => {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
