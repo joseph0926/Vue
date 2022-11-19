e952fd7f352c592500d6ed141cdb31fdaea816c9
김영훈 <joseph0926@naver.com>
2022-11-19 11:25:49 +0900

    2022.11.19

---
+++ ch09/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <button @click="setSelectedComponent('ActiveGoalVue')">Active Goal</button>
+    <button @click="setSelectedComponent('ManageGoalVue')">Manage Goal</button>
+    <!-- <ActiveGoalVue v-if="selectedComponent === 'ActiveGoalVue'"></ActiveGoalVue>
+    <ManageGoalVue v-if="selectedComponent === 'ManageGoalVue'"></ManageGoalVue> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeaderVue from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import ActiveGoalVue from "./components/ActiveGoal.vue";
+import ManageGoalVue from "./components/ManageGoal.vue";
+
+export default {
+  components: {
+    "the-header": TheHeaderVue,
+    "badge-list": BadgeList,
+    "user-info": UserInfo,
+    ActiveGoalVue,
+    ManageGoalVue,
+  },
+  data() {
+    return {
+      selectedComponent: "ActiveGoalVue",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ ch09/src/components/ManageGoal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Manage Goal</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <Teleport to="body">
+      <ErrorModalVue v-if="inputIsInvalid">
+        <h2>입력이 유효하지 않습니다!</h2>
+        <p>올바르게 다시 입력해주세요~</p>
+        <button @click="confirmError">OK</button>
+      </ErrorModalVue>
+    </Teleport>
+  </div>
+</template>
+
+<script>
+import ErrorModalVue from "./ErrorModal.vue";
+
+export default {
+  components: {
+    ErrorModalVue,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
